f14e0412e80f70360944f07de75d16f59d1e1f40
Mohammad Monfared <monfared.mohammad@gmail.com>
2024-06-26 09:47:05 -0400

    second simple todo list is done

--- scripts/11-todo-list.js
+++ scripts/11-todo-list.js
@@ -1,12 +1,39 @@
-const todoList = [];
+const todoList1 = [];
+const todoList2 = [];
 
-function addTodo() {
-  const inputElement = document.querySelector('.js-name-input');
+function addTodo1() {
+  const inputElement = document.querySelector('.js-name-input-1');
   const name = inputElement.value;
   console.log(name);
 
-  todoList.push(name);
-  console.log(todoList);
+  todoList1.push(name);
+  console.log(todoList1);
 
   inputElement.value = '';
+}
+
+function addTodo2() {
+  const inputElement = document.querySelector('.js-name-input-2');
+  const name = inputElement.value;
+  console.log(name);
+
+  todoList2.push(name);
+  console.log(todoList2);
+
+  inputElement.value = '';
+
+  renderTodoList();
+}
+
+function renderTodoList() {
+  let todoListHTML = '';
+
+  for (let i = 0; i < todoList2.length; i++) {
+    const todo = todoList2[i];
+    const html = `<p>${todo}</p>`;
+    todoListHTML += html;
+  }
+  console.log(todoListHTML);
+
+  document.querySelector('.js-todo-list').innerHTML = todoListHTML;
 }
